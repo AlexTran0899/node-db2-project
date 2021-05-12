fef5660fff8c271d591ee40223ca7f81face1905
alextran0899 <alextran@MacBook-Air.local>
2021-05-11 20:15:51 -0500

    done

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,65 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const Cars = require('./cars-model')
+const yup = require('yup')
+const vinValidator = require('vin-validator')
+
+const messageSchema = yup.object({
+  vin: yup.string()
+    .trim()
+    .required('vin is missing'),
+  make: yup.string()
+    .trim()
+    .required('make is missing'),
+  model: yup.string()
+    .trim()
+    .required('model is missing'),
+  mileage: yup.number()
+    .required('mileage is missing'),
+  title: yup.string(),
+  transmission: yup.string(),
+})
+
+exports.checkCarId = async (req, res, next) => {
+  try {
+    const car = await Cars.getById(req.params.id)
+    if (!car) {
+      next({ status: 404, message: `car with the id of ${req.params.id} not found` })
+    } else {
+      req.car = car
+      next()
+    }
+
+  } catch (err) {
+    next(err)
+  }
 }
 
-const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkCarPayload = async (req, res, next) => {
+  try {
+    const validate = await messageSchema.validate(req.body, { stripUnknown: true })
+    req.body = validate
+    next()
+  } catch (err) {
+    next({ status: 400, message: err.message })
+  }
 }
 
-const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkVinNumberValid = (req, res, next) => {
+  if (vinValidator.validate(req.body.vin)) {
+    next()
+  } else {
+    next({ status: 400, message: `vin ${req.body.vin} is invalid` })
+  }
 }
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkVinNumberUnique = (req, res, next) => {
+  Cars.getByVin(req.body.vin)
+    .then(data => {
+      if (data) {
+        next({ status: 400, message: `vin ${req.body.vin} already exists` })
+      } else {
+        next()
+      }
+    })
+    .catch(next)
 }
+
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,11 @@
-// STRETCH
+exports.seed = async function (knex) {
+    // Truncates table - wipes it, resets primary key to 1
+    await knex('cars').truncate()
+
+    // Inserts seed entries
+    await knex('cars').insert([
+        { vin: '3LNHL2JC6BR761449', make: 'acura', model: 'tl', mileage: '189922', title: 'fun on a budget', transmission: '6 speed auto' },
+        { vin: '3LNHL2JC6BR761409', make: 'toyota', model: 'highlander', mileage: '150900', title: 'the reliable subaru', transmission: '6 speed auto' },
+        { vin: '3LNHL2JC6BR761119', make: 'toyota', model: 'prius', mileage: '10000', title: 'dave ramesy for cars', transmission: 'cvt' }
+    ])
+}
